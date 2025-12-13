5567f1a6cf09c9a98b264d09081f6be7305b36da
astrobert88 <astrobert88@users.noreply.github.com>
2025-12-13 21:54:36 +0100

    Set display to 64x32 resolution with proper text positioning

--- src/main.cpp
+++ src/main.cpp
@@ -3,43 +3,37 @@
 #include <Wire.h>
 
 #define SCREEN_WIDTH 64
-#define SCREEN_HEIGHT 48
+#define SCREEN_HEIGHT 32
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
 void setup()
 {
+  delay(1000);
+
   Wire.begin(5, 6);
   delay(100);
 
   if (display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
   {
-    // Clear multiple times to ensure all pixels are off
     display.clearDisplay();
     display.display();
     delay(100);
 
-    display.clearDisplay();
-    display.display();
-    delay(100);
-
-    // Now draw the text
     display.clearDisplay();
     display.setTextSize(1);
     display.setTextColor(SSD1306_WHITE);
+
     display.setCursor(0, 0);
-    display.println("Hello");
-    display.println("World!");
+    display.print("Lights");
+
+    display.setCursor(0, 12);
+    display.print("On:  18:30");
+
+    display.setCursor(0, 24);
+    display.print("Off: 23:00");
+
     display.display();
-  }
-  else
-  {
-    pinMode(2, OUTPUT);
-    while (1)
-    {
-      digitalWrite(2, !digitalRead(2));
-      delay(300);
-    }
   }
 }
 
